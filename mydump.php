307b0219b68fbe6c97c8ed8b1b2c3ccd3935b775
borracciaBlu <marco.asdman@gmail.com>
2013-05-12 00:38:04 +0100

    First import

---
+++ mydump.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Script to backup a db in local using mysqldump in a unix system
+ */
+$dbuser = 'dbUser';
+$dbpass = 'dbPass';
+$backup_dir = '~/Dir';
+
+$settings = array (
+    array("db" => "db_to_backup1"),
+    array("db" => "db_to_backup2"),
+);
+
+echo "Start dump: \n";
+foreach ($settings as $backup_details) {
+    $db   = $backup_details['db'];
+    $date = date(DATE_ISO8601); 
+    
+    $cmd  = "mysqldump -u{$dbuser} -p{$dbpass} $db > {$backup_dir}{$date}_{$db}.sql";
+    $result = exec($cmd);
+    
+    echo $cmd . "\n";
+    if (!empty($result)) {
+        echo $result . "\n";
+    }
+}
